0261f060f70fa268d77a53bd0261dcfd0524beb8
Alfredo Baldoceda <alfredo.baldoceda@nomissolutions.com>
2022-11-03 05:45:26 -0400

    testing ingress

--- prime.py
+++ prime.py
@@ -1,15 +1,25 @@
 import uuid
-from flask import Flask
+from flask import Flask, jsonify, request
 
 instance_id = uuid.uuid4().hex
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def get_instance_id():
-    return f"Instance ID: {instance_id}"
+    if (request.method == "POST"):
+        some_json = request.get_json()
+        return jsonify({'Processing': some_json})
+    else:
+        return jsonify({'InstanceID': instance_id,
+                        'Message': 'Autobots, fall back!'})
+
+
+@app.route('/number/<int:num>', methods=['GET'])
+def is_prime(num):
+    return jsonify({'result': num*10})
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+    app.run(port=5000, host="0.0.0.0", debug=True)
